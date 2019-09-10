48bafb4ecd7563a339d9562bf9c3c780cb1b0b28
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-09-11 05:22:57 +0700

    add layout landing and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello</h1>
+        <Navbar />
+        <Landing />
+      </div>
+    );
+  }
 }
 
 export default App;
