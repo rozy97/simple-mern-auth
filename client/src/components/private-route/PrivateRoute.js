8aa69660570d23922cd8308deb129081fab6c012
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-09-14 23:59:43 +0700

    private route final

--- client/src/components/private-route/PrivateRoute.js
+++ client/src/components/private-route/PrivateRoute.js
@@ -1,4 +1,4 @@
-import React, from "react";
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
@@ -15,3 +15,13 @@ const privateRoute = ({ component: Component, auth, ...rest }) => (
     }
   />
 );
+
+privateRoute.PropTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(privateRoute);
