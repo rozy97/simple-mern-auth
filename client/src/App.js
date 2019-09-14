94e3f16eb929b567339837e4ece17d9856098e38
Firmansyah rozy <rozy.firmansyah@gmail.com>
2019-09-14 22:01:07 +0700

    add auth login and register js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/Register";
+import Login from "./components/auth/Login";
 
 class App extends Component {
   state = {};
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-        <Navbar />
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <h1>Hello</h1>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
