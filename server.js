b383beee75bde1a6e0fb07e6452af1a49ecfce14
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-10-25 07:21:04 +0700

    deploy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const cors = require("cors");
+const path = require("path");
 
 const users = require("./routes/api/users");
 const pokemon = require("./routes/api/pokemon");
@@ -35,5 +36,12 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/pokemon", pokemon);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
