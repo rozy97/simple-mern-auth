d00c2eb47542cb9afaeff333edaf6e498a6c5dd8
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2019-10-25 08:43:07 +0700

    tes deploy aws

---
+++ client/build/precache-manifest.d57c60859df77164147bae6bf9a7dce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3275e7603d159ca12b0eb523e12cae4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2011db7a7588aea1acb",
+    "url": "/static/css/main.a59295ec.chunk.css"
+  },
+  {
+    "revision": "7c2876268a977bb9611c",
+    "url": "/static/js/2.2853b96c.chunk.js"
+  },
+  {
+    "revision": "e2011db7a7588aea1acb",
+    "url": "/static/js/main.f5848649.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "60a82c6cf7fda046b291e6b2c78ea531",
+    "url": "/static/media/cloud.60a82c6c.png"
+  }
+]);
